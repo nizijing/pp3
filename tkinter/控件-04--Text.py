f04b8de38cca82262d4be65626c630aacd1e8474
nizj <nizj@pukkasoft.cn>
2020-10-09 19:49:13 +0800

    调整间距

--- tkinter/控件-04--Text.py
+++ tkinter/控件-04--Text.py
@@ -22,9 +22,9 @@ import time
 import datetime
 
 def gettime():
-       s=str(datetime.datetime.now())+'\n'
-       txt.insert(END,s)
-       root.after(1000,gettime)  # 每隔1s调用函数 gettime 自身获取时间
+    s=str(datetime.datetime.now())+'\n'
+    txt.insert(END,s)
+    root.after(1000,gettime)  # 每隔1s调用函数 gettime 自身获取时间
 
 root=Tk()
 root.geometry('320x240')
