1842219cb4b63ad2adb79c14dfc610d08a289068
nizj <nizj@pukkasoft.cn>
2020-11-28 17:05:23 +0800

    p2

---
+++ cookbook/3-数字日期和时间/p03_格式化输出数字.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+###################################################
+# Created : 2020-11-28 17:04:37
+# Author : zijing (zijing412@163.com)
+###################################################
+"""
+Topic: 格式化输出数字
+Desc : 
+"""
+
+
+def format_number():
+    x = 1234.56789
+    # Two decimal places of accuracy
+    print(format(x, '0.2f'))
+
+    # Right justified in 10 chars, one-digit accuracy
+    print(format(x, '>10.1f'))
+
+    # Left justified
+    print(format(x, '<10.1f'))
+
+    # Centered
+    print(format(x, '^10.1f'))
+
+    # Inclusion of thousands separator
+    print(format(x, ','))
+    print(format(x, '0,.1f'))
+
+    print(format(x, 'e'))
+    print(format(x, '0.2E'))
+
+    # strings
+    print('The value is {:0,.2f}'.format(x))
+
+    print(format(x, '0.1f'))
+    print(format(-x, '0.1f'))
+
+    swap_separators = {ord('.'): ',', ord(','): '.'}
+    print(format(x, ',').translate(swap_separators))
+
+
+if __name__ == '__main__':
+    format_number()
+
+
